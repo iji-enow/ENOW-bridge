aa49929af7a13546308d8611cf7c30284f8f40a4
Lee gun joon <kirkee2@naver.com>
2016-08-31 18:34:31 +0900

    saving for format

--- ascoltatori/start.js
+++ ascoltatori/start.js
@@ -16,7 +16,8 @@ var settings_kafka = {
   type: 'kafka',
   json: false,
   kafka: require('kafka-node'),
-  connectString: "localhost:2181",
+  //connectString: "192.168.0.3:2181",
+  connectString: "127.0.0.1:2181",
   clientId: "ascoltatori",
   groupId: "ascoltatori",
   defaultEncoding: "utf8",
@@ -28,19 +29,24 @@ var settings_kafka = {
 
 var request = require('request');
 
-var options = {
-  uri: 'https://hooks.slack.com/services/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',
-  method: 'POST',
-  json: {
-    "text": "good"
-  }
-};
-
 ascoltatori_mqtt.build(settings_mqtt, function (err, ascoltatori_mqtt){
   ascoltatori_mqtt.subscribe('test', function(topic,message) {
     console.log(arguments);
     ascoltatori_kafka.build(settings_kafka, function (err, ascoltatori_kafka){
       ascoltatori_kafka.publish('test', message, function() {
+        console.log('message published');
+      });
+    });
+  });
+});
+
+ascoltatori_mqtt.build(settings_mqtt, function (err, ascoltatori_mqtt){
+  ascoltatori_mqtt.subscribe('enow/+/+/+/+', function(topic,message) {
+    //    enow/serverId/phaseNum/action/brokerId/deviceId/parameter
+    console.log(arguments);
+    var topicName = JSON.stringify(arguments[0]);
+    ascoltatori_kafka.build(settings_kafka, function (err, ascoltatori_kafka){
+      ascoltatori_kafka.publish('event', topicName.substring(1,topicName.length -1) + " " + message, function() {
         console.log('message published');
       });
     });
